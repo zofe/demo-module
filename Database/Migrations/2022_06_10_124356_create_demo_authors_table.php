609f1c4dfc0a21c9e9fd0b4a05e17716376b26bb
Felice <felice.ostuni@uania.com>
2022-11-30 09:10:19 +0100

    fix

--- Database/Migrations/2022_06_10_124356_create_demo_authors_table.php
+++ Database/Migrations/2022_06_10_124356_create_demo_authors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAuthorsTable extends Migration
+class CreateDemoAuthorsTable extends Migration
 {
     /**
      * Run the migrations.
